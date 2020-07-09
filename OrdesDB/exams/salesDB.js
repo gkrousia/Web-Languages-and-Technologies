d58198ae2fbc84afcc9cd102dadc89a83e684ab6
gkrousia <61472705+gkrousia@users.noreply.github.com>
2020-07-09 13:47:43 +0300

    Add files via upload

---
+++ OrdesDB/exams/salesDB.js
@@ -0,0 +1,40 @@
+var customers = new Array();
+
+	customers[0] = "Σαλαμπάσης Μιχάλης";
+	customers[1] = "Τεκτονίδης Δημήτρης";
+	customers[2] = "Κουρουπέτρογλου Χρήστος";
+
+var imagessrc = "http://erodios.it.teithe.gr/intprog/exams/";
+
+var products = new Array();
+
+	products[0] = ["Printer 1", imagessrc+ "printer1.JPG", 150];
+	products[1] = ["Printer 2", imagessrc+ "printer2.JPG", 160];
+	products[2] = ["Printer 3", imagessrc+ "printer3.JPG", 170];
+	products[3] = ["Hard Disk 1", imagessrc+ "HDD1.JPG", 50];
+	products[4] = ["Hard Disk 2", imagessrc+ "HDD2.JPG", 60];
+	products[5] = ["Hard Disk 3", imagessrc+ "HDD3.JPG", 70];
+	products[6] = ["Laptop 1", imagessrc+ "laptop1.JPG", 700];
+	products[7] = ["Laptop 2", imagessrc+ "laptop2.JPG", 800];
+	products[8] = ["Laptop 3", imagessrc+ "laptop3.JPG", 900];
+	products[9] = ["Monitor 1", imagessrc+ "monitor1.JPG", 200];
+	products[10] = ["Monitor 2", imagessrc+ "monitor2.JPG", 250];
+	products[11] = ["Monitor 3", imagessrc+ "monitor3.JPG", 300];
+
+
+var sales = new Array();
+// customerID, ProductID, Quantity
+	sales[0] = [0,1,1]
+	sales[1] = [0,2,1]
+	sales[2] = [0,2,2]
+	sales[3] = [0,3,2]
+	sales[4] = [1,3,3]
+	sales[5] = [1,4,1]
+	sales[6] = [1,5,2]
+	sales[7] = [1,6,1]
+	sales[8] = [1,7,1]
+	sales[9] = [1,11,1]
+	sales[10] = [2,7,1]
+	sales[11] = [2,8,2]
+	sales[12] = [2,9,3]
+	sales[13] = [2,10,1]
---
+++ OrdesDB/orders.js
@@ -0,0 +1,22 @@
+var orders = [
+    {customer_id:111, order_id: 21, price:200 ,product_id:31},
+    {customer_id:111, order_id: 21, price:300 ,product_id:32},
+    {customer_id:122, order_id: 22, price:700 ,product_id:32},
+    {customer_id:133, order_id: 23, price:400 ,product_id:33},
+    {customer_id:122, order_id: 22, price:400 ,product_id:33},
+    {customer_id:122, order_id: 22, price:200 ,product_id:31},
+    {customer_id:133, order_id: 23, price:700 ,product_id:32}
+];
+
+var customers= [
+ {customer_name: "George" ,id:111},
+ {customer_name: "Mary", id:122},
+ {customer_name: "Olga", id:133}
+];
+
+
+var products= [
+{product_id:31 ,product_name:"milk"},
+{product_id:32 ,product_name:"honey"},
+{product_id:33 ,product_name:"sugar"}
+];
